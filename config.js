ee42267aa3ce48d2f65e282e9d96f1fcb560993e
Ivan Cotacte <cotactearmenion@gmail.com>
2024-01-28 23:41:06 +0800

    Version: 1.0.0

---
+++ config.js
@@ -0,0 +1,6 @@
+module.exports = {
+    prefix: "/",
+    banned: [],
+    admins: ["100050076673558", "100090433611902"],
+  };
+  
---
+++ index.js
@@ -0,0 +1,39 @@
+const login = require('./fb-chat-api')
+const Server = require("@igorkowalczyk/repl-uptime");
+const dotenv = require('dotenv');
+const fs = require("fs");
+const config = require("./config");
+dotenv.config();
+
+login({ appState: JSON.parse(fs.readFileSync("appstate.json", "utf8")) }, (err, api) => {
+  if (err) return console.error(err);
+  api.setOptions({
+    logLevel: "silent",
+    forceLogin: true,
+    listenEvents: true,
+    autoMarkDelivery: false,
+    selfListen: true,
+    proxy: process.env.PROXY,
+  });
+
+  api.listenMqtt(async (err, event) => {
+    if (err) return log.error(err);
+
+    switch (event.type) {
+      case "message":
+      case "message_reply":
+        require("./handlers/handleMessage.js")({ api, event, config });
+        break;
+      case "event":
+        console.log(event);
+        break;
+    }
+  });
+});
+
+new Server({
+  port: 8080,
+  path: "/",
+  message: "🤙 Don't let your repl go to sleep!",
+  debug: true,
+});
