826c3a2e0cc18406c572379954875db6ea05cea7
Ivan Cotacte <cotactearmenion@gmail.com>
2024-02-01 17:46:22 +0800

    Fix some code!

---
+++ cmds/noprefix/help.js
@@ -0,0 +1,77 @@
+module.exports = async ({ api, event }) => {
+    const { prefix } = config;
+    const fs = require("fs");
+    const path = require("path");
+    try {
+      async function readFilesInDirectory(directoryPath) {
+        try {
+          const files = await fs.promises.readdir(directoryPath);
+  
+          const jsFiles = files.filter((file) => {
+            const filePath = path.join(directoryPath, file);
+            const fileStat = fs.statSync(filePath);
+            return fileStat.isFile() && path.extname(filePath) === ".js";
+          });
+  
+          const fileNames = jsFiles.map((file) => path.parse(file).name);
+  
+          return fileNames;
+        } catch (error) {
+          console.error("Error reading directory:", error);
+          return [];
+        }
+      }
+  
+      let commandsArray = await readFilesInDirectory("./noprefix");
+  
+      let input = event.body;
+      let data = input.split(" ");
+      data.shift();
+      let query = data[0];
+  
+      function paginate(arr, pageLength) {
+        const numOfPages = Math.ceil(arr.length / pageLength);
+        const paginatedArray = [];
+  
+        for (let i = 0; i < numOfPages; i++) {
+          const start = i * pageLength;
+          const end = start + pageLength;
+          paginatedArray.push(arr.slice(start, end));
+        }
+        return paginatedArray;
+      }
+  
+      function show(arrList, n) {
+        try {
+          let r = paginate(arrList, 10);
+          let arr = [];
+          if (!n) {
+            n = 1;
+            query = 1;
+          }
+          let list = r[n - 1];
+          for (let i = 0; i < list.length; i++) {
+            arr.push(`• ${prefix}${list[i]}\n\n`);
+          }
+          return arr;
+        } catch (err) {
+          api.sendMessage(`Error: ${err}`, event.threadID, event.messageID);
+        }
+      }
+  
+      let qotd = await qt();
+      let result = show(commandsArray, query);
+      const numOfPages = paginate(commandsArray, 10).length;
+      api.sendMessage(
+        `｢Commands｣\n\n${result.join(
+          ""
+        )}\nPage » ${query}/${numOfPages}\nCommands » ${
+          commandsArray.length
+        }`,
+        event.threadID,
+        event.messageID
+      );
+    } catch (err) {
+      api.sendMessage(err, event.messageID, event.threadID);
+    }
+}
